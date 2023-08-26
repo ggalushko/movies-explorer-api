e39cdc413137d459466b3ad176f18c588152315e
ggalushko <titanmelchior@gmail.com>
2023-08-26 13:38:18 +0300

    -

---
+++ routes/movies.js
@@ -0,0 +1,33 @@
+const moviesRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const {
+  createMovie,
+  getMovies,
+  deleteMovie,
+} = require('../controllers/movies');
+
+moviesRouter.get('/', getMovies);
+
+moviesRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(/https?:\/\/(www\.)?[-\w@:%\.\+~#=]{1,256}\.[a-z0-9()]{1,6}\b([-\w()@:%\.\+~#=//?&]*)/i),
+    trailerLink: Joi.string().required().pattern(/https?:\/\/(www\.)?[-\w@:%\.\+~#=]{1,256}\.[a-z0-9()]{1,6}\b([-\w()@:%\.\+~#=//?&]*)/i),
+    thumbnail: Joi.string().required().pattern(/https?:\/\/(www\.)?[-\w@:%\.\+~#=]{1,256}\.[a-z0-9()]{1,6}\b([-\w()@:%\.\+~#=//?&]*)/i),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}), createMovie);
+
+moviesRouter.delete('/:_id', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().hex().length(24),
+  }),
+}), deleteMovie);
+
+module.exports = moviesRouter;
