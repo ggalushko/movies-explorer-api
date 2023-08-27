bf42e48c0c123257842981c9851c25c88a685786
ggalushko <titanmelchior@gmail.com>
2023-08-27 14:37:21 +0300

    required field

--- routes/movies.js
+++ routes/movies.js
@@ -27,7 +27,7 @@ moviesRouter.post('/', celebrate({
 
 moviesRouter.delete('/:_id', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().hex().length(24),
+    movieId: Joi.string().hex().length(24).required(),
   }),
 }), deleteMovie);
 
